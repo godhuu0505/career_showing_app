4d0e090f14a563dcbe57df59d1084b35a65aeba4
godhuu0505 <go.d.eye.0505@gmail.com>
2022-09-20 00:37:48 +0900

    change cats.controller.ts

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+}
